ec3389d401e5eef8b8d2632e2f338356a7d9c318
Bruno <brunoalexandersenai@gmail.com>
2026-03-13 21:17:32 -0300

    Add files via upload

---
+++ src/hexapod_ws/setup.bash
@@ -0,0 +1,4 @@
+source /opt/ros/jazzy/setup.bash
+source ~/tiffany_gazebo/install/setup.bash
+export GZ_SIM_RESOURCE_PATH=/opt/ros/jazzy/share:~/tiffany_gazebo/install/hexapod_ws/share
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libpthread.so.0
