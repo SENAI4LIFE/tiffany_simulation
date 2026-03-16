ee8ad1383ede104a4e4df6b52b7018f8b85d9157
Bruno <brunoalexandersenai@gmail.com>
2026-03-16 14:51:49 -0300

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -2,3 +2,5 @@ source /opt/ros/jazzy/setup.bash
 source ~/tiffany_gazebo/install/setup.bash
 export GZ_SIM_RESOURCE_PATH=/opt/ros/jazzy/share:~/tiffany_gazebo/install/hexapod_ws/share
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libpthread.so.0
+export MESA_GL_VERSION_OVERRIDE=3.3
+export LIBGL_ALWAYS_SOFTWARE=0
